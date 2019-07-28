73a8d00d1f9edf0c7fd93254291296be37cf53ff
mario <emmariodelar@gmail.com>
2019-07-29 00:18:13 +0200

    target elemnt on real time tweet

--- src/routes/endpoints/twitter.ts
+++ src/routes/endpoints/twitter.ts
@@ -1,27 +1,50 @@
-const Twitter = require("twitter");
+import { EventEmitter } from 'stream';
+import Twitter = require("twitter");
 
-var client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.TOKEN_KEY,
-  access_token_secret: process.env.TOKEN_SECRET
+const client: Twitter = new Twitter({
+  access_token_key: `${process.env.TOKEN_KEY}`,
+  access_token_secret: `${process.env.TOKEN_SECRET}`,
+  consumer_key: `${process.env.CONSUMER_KEY}`,
+  consumer_secret: `${process.env.CONSUMER_SECRET}`
 });
 
 // Tweeter Args
 const params: object = {track: 'twitter'};
-const path: String = 'statuses/filter';
+const path: string = 'statuses/filter';
 
 // Define Stream
-const stream = client.stream(path, params);
+const stream: EventEmitter = client.stream(path, params);
+
+// Function
+const parseChunkToObject: any = (data: any) => {
+  const element: object = 
+    (typeof data) === "object" ? data : {};
+
+  return element;
+}
+
+const onReceiveData = (event: Twitter.ResponseData) => {
+  // parse to object
+  const chunck: any = parseChunkToObject(event);
+  const tweetIsDefine: boolean = Object.prototype.hasOwnProperty.call(event, 'user');
+  
+  if(tweetIsDefine) {
+    // When i have get my object
+    process.stdout.write(JSON.stringify({
+      text: chunck.text,
+      username:  chunck.user.name
+    }, null, " "));
+  }
+};
+
+const onError = (error: any) => {
+  throw error;
+};
 
 // Manage stream
 
 // On success
-stream.on('data', (event: any) => {
-    console.log(event && event.text);
-});
+stream.on('data', onReceiveData);
 
 // On error
-stream.on('error', (error: any) => {
-    throw error;
-});
+stream.on('error', onError);
